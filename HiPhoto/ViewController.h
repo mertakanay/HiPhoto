b15331a50bea463c5be1f9e44f5aa36e63b0d911
mertakanay <mert.akanay@live.com>
2015-03-28 13:26:38 -0700

    Initial Commit

---
+++ HiPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HiPhoto
+//
+//  Created by Mert Akanay on 3/28/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
